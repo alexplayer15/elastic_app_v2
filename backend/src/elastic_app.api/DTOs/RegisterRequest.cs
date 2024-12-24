8ef4726e8597d8c3eafacaa3f29b29db47fea8a9
AlexPlayerEasyJet15 <Alex.Player@easyJet.com>
2024-12-24 10:55:59 +0000

    add in registration model

---
+++ backend/src/elastic_app.api/DTOs/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace elastic_app.api.DTOs
+{
+    public class RegisterRequest
+    {
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public string ReEnterPassword { get; set; }
+
+    }
+}
