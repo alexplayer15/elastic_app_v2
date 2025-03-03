8cc4c7fa375a977608f99937ab31fb456728885e
alexplayer15 <alexplayer15@icloud.com>
2025-03-03 18:43:46 +0000

    start adding in testcontainers implementation

---
+++ backend/test/elastic_app.integration.tests/Hooks/IntegrationTestHooks.cs
@@ -0,0 +1,46 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Testcontainers.LocalStack;
+using Reqnroll;
+using System.Net.Sockets;
+
+namespace elastic_app.integration.tests.Hooks
+{
+    public class IntegrationTestHooks
+    {
+        [BeforeTestRun]
+
+        public async Task BeforeFeature()
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Integration");
+
+            if (await LocalStackContainerExists())
+            {
+                return;
+            }
+
+            await SetUpLocalStack(); 
+        }
+
+        public async Task<bool> LocalStackContainerExists()
+        {
+            return false;
+        }
+        public async Task SetUpLocalStack()
+        {
+            var localStackContainer = new LocalStackBuilder()
+                .WithName(HookConstants.LocalStackTestContainerName)
+                .WithResourceMapping("../../localStackScripts", "etc/localstack/init/ready.d", UnixFileModes.UserExecute)
+                .WithPortBinding(4566, true)
+                .WithEnvironment("DYNAMODB_SHARE_DB", "1")
+                .WithEnvironment("AWS_ACCESS_KEY_ID", "DUMMYIDEXAMPLE")
+                .WithEnvironment("AWS_SECRET_ACCESS_KEY", "DUMMYEXAMPLEKEY")
+                .WithEnvironment("REGION", "eu-west-2")
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Token Data table populated"))
+                .Build();
+            await localStackContainer.StartAsync();
+        }
+    }
+}
