43cad2c7bd9ef9ea27a1bafba89568e480028a54
alexplayer15 <alexplayer15@icloud.com>
2025-03-29 11:32:21 +0000

    add in basic vpc to test if pipeline works

--- terraform/dynamoDb/main.tf
+++ terraform/dynamoDb/main.tf
@@ -1,20 +1,20 @@
-resource "aws_dynamodb_table" "users_table" {
-  name           = "UserData"              
-  billing_mode   = "PAY_PER_REQUEST" 
-  hash_key       = "id"                 
+# resource "aws_dynamodb_table" "users_table" {
+#   name           = "UserData"              
+#   billing_mode   = "PAY_PER_REQUEST" 
+#   hash_key       = "id"                 
 
-  attribute {
-    name = "id"                          
-    type = "S"                           
-  }
+#   attribute {
+#     name = "id"                          
+#     type = "S"                           
+#   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = true
-  }
+#   ttl {
+#     attribute_name = "TimeToExist"
+#     enabled        = true
+#   }
 
-  tags = {
-    Name        = "UserData"
-    Environment = "production"
-  }
-}
+#   tags = {
+#     Name        = "UserData"
+#     Environment = "production"
+#   }
+# }
