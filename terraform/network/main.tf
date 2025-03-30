8fd4d63e49f77d3d2b49760551d5c5e4e94fa78c
alexplayer15 <alexplayer15@icloud.com>
2025-03-30 15:56:41 +0100

    correct route to allow internet traffic

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -27,7 +27,7 @@ resource "aws_route_table" "main_pub_sub_route_table" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0..0/0"
     gateway_id = aws_internet_gateway.main_vpc_igw.id
   }
 }
