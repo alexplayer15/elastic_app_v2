d1e63022d6a81994984f5bd864829e5509bad472
alexplayer15 <alexplayer15@icloud.com>
2025-03-30 15:47:55 +0100

    add in infra for public subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -1,7 +1,38 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
 
   tags = {
-    Name = "elastic_app_v2"
+    Name = "main_vpc"
   }
+}
+
+resource "aws_subnet" "alb_public_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "ALB_pub_sub"
+  }
+}
+
+resource "aws_internet_gateway" "main_vpc_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main_pub_sub_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.main_vpc_igw.id
+  }
+}
+
+resource "aws_route_table_association" "alb_pub_sub_assocation" {
+  subnet_id      = aws_subnet.alb_public_subnet.id
+  route_table_id = aws_route_table.main_pub_sub_route_table.id
 }
