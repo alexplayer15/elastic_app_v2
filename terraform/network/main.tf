7dc3f6f1ff76d9c332bdc258e4eac80fa7fb10cd
alexplayer15 <alexplayer15@icloud.com>
2025-03-30 16:42:14 +0100

    start adding in private network

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -1,3 +1,5 @@
+//public networks
+
 resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
 
@@ -36,3 +38,41 @@ resource "aws_route_table_association" "alb_pub_sub_assocation" {
   subnet_id      = aws_subnet.alb_public_subnet.id
   route_table_id = aws_route_table.main_pub_sub_route_table.id
 }
+
+//Private networks
+
+resource "aws_subnet" "ecs_private_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "ECS_priv_sub"
+  }
+}
+
+resource "aws_route_table" "main_priv_sub_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table_association" "ecs_priv_sub_association" {
+  subnet_id      = aws_subnet.ecs_private_subnet.id
+  route_table_id = aws_route_table.main_priv_sub_route_table.id
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.eu-west-2.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_id = aws_route_table.main_priv_sub_route_table.id
+
+  tags = {
+    Name = "DynamoDB VPC Endpoint"
+  }
+}
+
+# resource "aws_vpc_endpoint" "ecr_interface_vpc_endpoint" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.eu-west-2.ecr"
+#   vpc_endpoint_type = "Interface"
+# }
+
