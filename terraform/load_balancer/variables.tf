eba0785ced7ea519307afc37fcb920a7b23cb813
alexplayer15 <alexplayer15@icloud.com>
2025-03-30 17:53:40 +0100

    add in sg id var

--- terraform/load_balancer/variables.tf
+++ terraform/load_balancer/variables.tf
@@ -2,3 +2,8 @@ variable "ecs_priv_sub_id"{
     type = string 
     description = "Private subnet id ECS tasks are in"
 }
+
+variable "alb_sg_id"{
+    type = string 
+    description = "ALB security group ID"
+}
